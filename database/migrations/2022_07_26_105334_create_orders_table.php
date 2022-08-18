a1ae39ec8b3947c11edf00bce37af1a7f0d017cf
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-08-18 16:00:19 +0800

    edit transcontroller

--- database/migrations/2022_07_26_105334_create_orders_table.php
+++ database/migrations/2022_07_26_105334_create_orders_table.php
@@ -1,35 +1,35 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
-{
-    // /**
-    //  * Run the migrations.
-    //  *
-    //  * @return void
-    //  */
-    // public function up()
-    // {
-    //     Schema::create('orders', function (Blueprint $table) {
-    //         $table->increments('id');
-    //         $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
-    //         $table->foreignId('product_category_id')->constrained()->onDelete('cascade');;
-    //         $table->string('size');
-    //         $table->integer('qty');
-    //         $table->timestamps();
-    //     });
-    // }
+// class CreateOrdersTable extends Migration
+// {
+//     // /**
+//     //  * Run the migrations.
+//     //  *
+//     //  * @return void
+//     //  */
+//     // public function up()
+//     // {
+//     //     Schema::create('orders', function (Blueprint $table) {
+//     //         $table->increments('id');
+//     //         $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
+//     //         $table->foreignId('product_category_id')->constrained()->onDelete('cascade');;
+//     //         $table->string('size');
+//     //         $table->integer('qty');
+//     //         $table->timestamps();
+//     //     });
+//     // }
 
-    // /**
-    //  * Reverse the migrations.
-    //  *
-    //  * @return void
-    //  */
-    // public function down()
-    // {
-    //     Schema::dropIfExists('orders');
-    // }
-}
+//     // /**
+//     //  * Reverse the migrations.
+//     //  *
+//     //  * @return void
+//     //  */
+//     // public function down()
+//     // {
+//     //     Schema::dropIfExists('orders');
+//     // }
+// }
--- database/migrations/2022_08_15_154522_create_transactions_table.php
+++ database/migrations/2022_08_15_154522_create_transactions_table.php
@@ -1,37 +1,37 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        // Schema::create('transactions', function (Blueprint $table) {
-        //     $table->increments('id');
-        //     $table->string('trans_code');
-        //     $table->string('user_add');
-        //     $table->decimal('total_payment', 8, 2);
-        //     $table->string('payment_opt');
-        //     $table->string('status')->nullable()->default('Approval');
-        //     $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        //     $table->timestamps();
-        // });
-    }
+// class CreateTransactionsTable extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         // Schema::create('transactions', function (Blueprint $table) {
+//         //     $table->increments('id');
+//         //     $table->string('trans_code');
+//         //     $table->string('user_add');
+//         //     $table->decimal('total_payment', 8, 2);
+//         //     $table->string('payment_opt');
+//         //     $table->string('status')->nullable()->default('Approval');
+//         //     $table->foreignId('user_id')->constrained()->onDelete('cascade');
+//         //     $table->timestamps();
+//         // });
+//     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('transactions');
-    }
-}
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         Schema::dropIfExists('transactions');
+//     }
+// }
