5052cf0bad9d88d522a8130450caa4ad34e3901c
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-08-21 14:10:45 +0800

    product store

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -36,7 +36,7 @@ class ProductCategoryController extends Controller
             'name' => 'required|string',
             'image' => 'image|mimes:jpg,jpeg,png',
         ]);
-        $name = ProductCategory::where('name', $products['name']);
+        $name = ProductCategory::where('name', $request['name']);
         if ($request->hasFile('image') && !$name) {
             $filename = Str::random(10);
             $request->file('image')->storeAs('', $filename, 'google');
