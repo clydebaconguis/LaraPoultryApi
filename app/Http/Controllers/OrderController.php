75a6b5c3f09033957170d4af3a9fd9efc7c5d020
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-20 19:08:10 +0800

    order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,7 +15,7 @@ class OrderController extends Controller
     public function show($transaction_id)
     {
         return DB::table('orders')
-            ->join('product_categories', 'orders.product_category_id', "=", 'product_categries.id')
+            ->join('product_categories', 'orders.product_category_id', "=", 'product_categories.id')
             ->select('orders.*', 'product_categories.name', 'product_categories.image')
             ->where('transaction_id', $transaction_id)->get();
     }
