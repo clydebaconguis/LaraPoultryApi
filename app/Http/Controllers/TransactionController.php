219a423c0584ae20efbf634f267a1866dba00d4f
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-20 15:13:52 +0800

    transac

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Models\Transaction;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
