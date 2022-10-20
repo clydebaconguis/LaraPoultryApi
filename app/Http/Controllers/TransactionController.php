8f723f7942bef24bb8dfdd758f1c56d75e109a59
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-20 15:27:36 +0800

    transac

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -64,7 +64,7 @@ class TransactionController extends Controller
      */
     public function show($id)
     {
-        $transac = Transaction::where('user_id', $id)->get();
+        $transac = Transaction::where('user_id', $id)->id->get();
 
         return DB::table('orders')
             ->join('product_categories', 'orders.product_category_id', "=", 'product_categries.id')
