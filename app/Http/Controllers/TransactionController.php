5cd30e47b7912ea57787849cfb84a13962760418
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-20 16:14:30 +0800

    transac

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -68,12 +68,11 @@ class TransactionController extends Controller
         $Orders = [];
 
         foreach ($transac as $item) {
-            $transac_id = $item[id];
 
             $order = DB::table('orders')
                 ->join('product_categories', 'orders.product_category_id', "=", 'product_categries.id')
                 ->select('orders.*', 'product_categories.name', 'product_categories.image')
-                ->where('transaction_id', $transac_id)->get();
+                ->where('transaction_id', $item['id'])->get();
 
             array_push($Orders, $order);
         }
