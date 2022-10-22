8202b1b1e75d8eab2f69a251d8d2dce0f2d63cca
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-22 18:06:23 +0800

    user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     public function getUsers()
     {
-        return User::orderBy('created_at', 'ASC')->groupBy('status')->get();
+        return User::groupBy('status')->orderBy('created_at', 'ASC')->get();
     }
 
     public function verify($id)
