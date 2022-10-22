984dcb038767380662d904d1008ca83de41339a8
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-22 17:34:14 +0800

    transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -22,7 +22,9 @@ class TransactionController extends Controller
 
         return DB::table('transactions')
             ->join('users', 'transactions.user_id', "=", 'users.id')
-            ->select('transactions.*', 'users.name')->get();
+            ->select('transactions.*', 'users.name')
+            ->where('status', 'for approval')
+            ->orderBy('created_at', 'DESC')->get();
     }
 
     /**
