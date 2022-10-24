436da3dd13f741a36b79cb0895412ac31d98edd4
Clyde Baconguis <clydebaconguis3737@gmail.com>
2022-10-25 00:33:40 +0800

    transact

--- database/migrations/2022_10_24_163023_add_phone_columns_to_transactions_table.php
+++ database/migrations/2022_10_24_163023_add_phone_columns_to_transactions_table.php
@@ -14,7 +14,7 @@ class AddPhoneColumnsToTransactionsTable extends Migration
     public function up()
     {
         Schema::table('transactions', function (Blueprint $table) {
-            //
+            $table->string('phone');
         });
     }
 
