1f2ddd671d0f80f591fb0ce588202bba5abe8099
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-04 10:58:04 +0800

    secure

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,13 +10,17 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
+    @production
     <link href="{{secure_asset('admin/vendor/fontawesome-free/css/all.min.css')}}" type="text/css" rel="stylesheet">
+    @endproduction
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
+    @production
     <!-- Custom styles for this template-->
     <link href="{{secure_asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    @endproduction
 
 </head>
 <body class="bg-gradient-primary">
