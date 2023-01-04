7801debb74a4bdb7eb465867641a10e517653267
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-04 11:51:14 +0800

    asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,14 +9,12 @@
 
     <title>SB Admin 2 - Dashboard</title>
 
-    @production
         
     <!-- Custom fonts for this template-->
     <link href="{{asset('admin/vendor/fontawesome-free/css/all.min.css')}}" type="text/css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
-    @endproduction
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -27,7 +25,6 @@
     <div class="container">
         @yield('content')
     </div>
-    @production
      <!-- Bootstrap core JavaScript-->
      <script src="{{asset('admin/vendor/jquery/jquery.min.js')}}"></script>
      <script src="{{asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -37,6 +34,5 @@
  
      <!-- Custom scripts for all pages-->
      <script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
-     @endproduction
 </body>
 </html>
