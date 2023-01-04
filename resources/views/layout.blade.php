f1ebbd420363d91e88d10f9e99f71e13815fe96b
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-04 12:27:40 +0800

    asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,14 +11,14 @@
 
         
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('https://larapoultry.herokuapp.com/admin/vendor/fontawesome-free/css/all.min.css')}}" type="text/css" rel="stylesheet">
+    <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css')}}" type="text/css" rel="stylesheet">
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('https://larapoultry.herokuapp.com/admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 </head>
 <body class="bg-gradient-primary">
@@ -27,13 +27,13 @@
         @yield('content')
     </div>
      <!-- Bootstrap core JavaScript-->
-     <script src="{{ asset('https://larapoultry.herokuapp.com/admin/vendor/jquery/jquery.min.js')}}"></script>
-     <script src="{{ asset('https://larapoultry.herokuapp.com/admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+     <script src="{{ asset('admin/vendor/jquery/jquery.min.js')}}"></script>
+     <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
  
      <!-- Core plugin JavaScript-->
-     <script src="{{ asset('https://larapoultry.herokuapp.com/admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+     <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
  
      <!-- Custom scripts for all pages-->
-     <script src="{{ asset('https://larapoultry.herokuapp.com/admin/js/sb-admin-2.min.js')}}"></script>
+     <script src="{{ asset('admin/js/sb-admin-2.min.js')}}"></script>
 </body>
 </html>
