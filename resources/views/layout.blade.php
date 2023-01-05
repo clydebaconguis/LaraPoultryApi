2d38968732b0b32567fdbb62cc066b29f408cd8f
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-05 11:37:36 +0800

    asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -27,14 +27,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('css/assets/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('css/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('css/assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
+    <script src="{{asset('css/assets/js/sb-admin-2.min.js')}}"></script>
 
 </body>
 </html>
