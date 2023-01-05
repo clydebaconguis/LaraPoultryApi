ed2dfe8d456a8f99e6cfce79c93abecbf652f3ef
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-05 10:12:17 +0800

    asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS', true);
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
         }
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,14 +10,14 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link type="text/css" rel="stylesheet" href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{secure_asset('assets/vendor/fontawesome-free/css/all.min.css')}}">
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link rel="stylesheet" href="{{asset('assets/css/sb-admin-2.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/css/sb-admin-2.min.css')}}">
 
 </head>
 <body class="bg-gradient-primary">
@@ -35,6 +35,6 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
-    
+
 </body>
 </html>
