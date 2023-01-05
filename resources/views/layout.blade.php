d142226c12d4c951d44ce330dff5efd440afd3e6
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-05 09:40:10 +0800

    asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,14 +10,14 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link type="text/css" rel="stylesheet" href="{{secure_asset('admin/vendor/fontawesome-free/css/all.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}">
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link rel="stylesheet" href="{{secure_asset('admin/css/sb-admin-2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/sb-admin-2.min.css')}}">
 
 </head>
 <body class="bg-gradient-primary">
@@ -27,14 +27,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('admin/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
+    <script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
         
 </body>
 </html>
