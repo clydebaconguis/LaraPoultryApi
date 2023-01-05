2b77654e74e603650872186d94fc84fd61b51b4a
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-05 09:50:21 +0800

    asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,6 +28,7 @@
         @yield('content')
     </div>
 
+    @production
     <!-- Bootstrap core JavaScript-->
     <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -37,6 +38,6 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
-        
+    @endproduction
 </body>
 </html>
