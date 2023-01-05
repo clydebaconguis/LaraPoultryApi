ebec0d612bd902bf21d3c8b03861c8190712e766
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-01-05 11:00:57 +0800

    asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,14 +10,14 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link type="text/css" rel="stylesheet" href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('public/assets/vendor/fontawesome-free/css/all.min.css')}}">
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link rel="stylesheet" href="{{asset('assets/css/sb-admin-2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/assets/css/sb-admin-2.min.css')}}">
 
 </head>
 <body class="bg-gradient-primary">
