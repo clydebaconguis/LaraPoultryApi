34d8c956a92d1cf0ec93818f7c1c4556a324a975
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-02-18 14:51:01 +0800

    editprod

--- resources/views/content/editprod.blade.php
+++ resources/views/content/editprod.blade.php
@@ -24,7 +24,7 @@
                     class=""
                     required
                     name="name"
-                    value="{{}}"
+                    {{-- value="{{}}" --}}
                 />
 
                 @error('name')
