de1dd5e5bbf789e04f6583b572e55cc5a3569dcc
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-02-18 18:22:54 +0800

    editprod

--- resources/views/content/addprod.blade.php
+++ resources/views/content/addprod.blade.php
@@ -1,104 +1,128 @@
 <x-layout>
     <x-card>
         <header class="text-center">
-            <h2 class="text-center text-lg">
+            <h2 class="text-center text-lg mb-5">
                 Create a Product
             </h2>
         </header>
 
         <form method="POST" action="/addproduct" enctype="multipart/form-data">
+        <table>
             @csrf
              <input
                     type="hidden"
                     name="purpose"
                     value="add"
                 />
-            <div class="mb-2">
-                <label
-                    for="name"
-                    class="text-md"
-                    >Product Name</label
-                >
-                <input
-                    type="text"
-                    class=""
-                    required
-                    name="name"
-                    value="{{old('name')}}"
-                />
 
-                @error('name')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+            <tr class="mb-2">
+                <td>
+                    <label
+                        class="text-md"
+                        >Product Name</label
+                    >
+                </td>
 
-            <div class="mb-2">
-                <label for="stock" class="inline-block text-md mb-2"
-                    >Stocks</label
-                >
-                <input
-                    type="number"
-                    name="stock"
-                    value="{{old('stock')}}"
-                />
-                @error('stock')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+                <td>
+                    <input
+                        type="text"
+                        class=""
+                        required
+                        name="name"
+                        value="{{old('name')}}"
+                    />
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+                </td>
+            </tr>
 
-            <div class="mb-2">
-                <label
-                    for="type"
-                    class="inline-block text-md mb-2"
-                    >Type</label
-                >
-                <select class="form-select" aria-label="Default select example" id="type" name="type" class="p-2">
-                    <option selected>Select Types</option>
-                    @unless (count($types) == 0)           
-                         @foreach ($types as $type)
-                            <option value={{$type->id}}>{{$type->name}}</option>
-                        @endforeach
-                    @endunless
-                </select>
-            </div>
+            <tr class="mb-2">
+                <td>
+                    <label class="inline-block text-md mb-2"
+                        >Stocks</label
+                    >
+                </td>
 
-            <div class="mb-2">
-                <label
-                    for="price"
-                    class="inline-block text-md mb-2"
-                    >Prices</label>
+                <td>
+                    <input
+                        type="number"
+                        name="stock"
+                        value="{{old('stock')}}"
+                    />
+                    @error('stock')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+                </td>
+            </tr>
 
-                 <input
-                    type="number"
-                    name="price"
-                    value="{{old('price')}}"
-                />
-                <select class="form-select" aria-label="Default select example" id="unit" name="unit" class="p-2">
-                    <option selected>Select Unit</option>
-                    @unless (count($units) == 0)
-                        @foreach ($units as $unit)
-                                <option value={{$unit->unit}}>{{$unit->unit}}</option>
-                        @endforeach
-                    @endunless
-                </select>
-            </div>
+            <tr class="mb-2">
+                <td>
+                    <label
+                        for="type"
+                        class="inline-block text-md mb-2"
+                        >Type</label
+                    >
+                </td>
+                <td>
+                    <select class="form-select" aria-label="Default select example" id="type" name="type" class="p-2">
+                        <option selected>Select Types</option>
+                        @unless (count($types) == 0)           
+                            @foreach ($types as $type)
+                                <option value={{$type->id}}>{{$type->name}}</option>
+                            @endforeach
+                        @endunless
+                    </select>
+                </td>
+            </tr>
 
-            <div class="mb-6">
-                <label for="image" class="inline-block text-lg mb-2">
-                    Product Image
-                </label>
-                <input
-                    type="file"
-                    class="border border-gray-200 rounded p-2 w-full"
-                    name="image"
-                />
+            <tr class="mb-2">
+                <td>
+                    <label
+                        for="price"
+                        class="inline-block text-md mb-2"
+                        >Prices</label>
+                </td>
+                <td>
+                    <input
+                        type="number"
+                        name="price"
+                        value="{{old('price')}}"
+                    />
+                </td>
+                <td>
+                    <select class="form-select" aria-label="Default select example" id="unit" name="unit" class="p-2">
+                        <option selected>Select Unit</option>
+                        @unless (count($units) == 0)
+                            @foreach ($units as $unit)
+                                    <option value={{$unit->unit}}>{{$unit->unit}}</option>
+                            @endforeach
+                        @endunless
+                    </select>
+                </td>
+            </tr>
 
-                @error('image')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+            <tr class="mb-6">
+                <td>
+                    <label for="image" class="inline-block text-lg mb-2">
+                        Product Image
+                    </label>
+                </td>
 
-            <div class="mb-6">
+                <td>
+                    <input
+                        type="file"
+                        class="border border-gray-200 rounded p-2 w-full"
+                        name="image"
+                    />
+                    @error('image')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+                </td>
+            </tr>
+            </table>
+
+            <div class="mb-6 mt-4">
                 <button type="submit"
                     class="bg-dark text-white rounded py-2 px-4 hover:bg-black"
                 >
