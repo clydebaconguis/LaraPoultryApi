50e57be9359a9f45f44289e707349e9de98b96e3
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-02-22 10:02:47 +0800

    order stat

--- resources/views/content/orders.blade.php
+++ resources/views/content/orders.blade.php
@@ -19,6 +19,7 @@
                                 <th>Name</th>
                                 <th>Phone</th>
                                 <th>Total</th>
+                                <th>Status</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -28,6 +29,7 @@
                                 <th>Name</th>
                                 <th>Phone</th>
                                 <th>Total</th>
+                                <th>Status</th>
                                 <th>Action</th>
                             </tr>
                         </tfoot>
@@ -39,13 +41,16 @@
                                         <td>{{$order->name}}</td>
                                         <td>{{$order->phone}}</td>
                                         <td>{{$order->total_payment}}</td>
+                                        <td>{{$order->status}}</td>
                                         <td class="d-flex">
                                             <a type="button" class="btn btn-info" href="/">Details</a>
-                                            <form method="POST" action="/orderStat/{{$order->id}}" enctype="multipart/form-data">
+                                            <form method="POST" action="/orderStat" enctype="multipart/form-data">
+                                            @csrf
                                                 <div class="dropdown ml-1">
                                                     <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                         Action
                                                     </button>
+                                                    <input type="hidden" value={{$order->id}} name="orderid" />
                                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                                         <a class="dropdown-item" name="ordered" type="submit" value="ordered" >Ordered</a>
                                                         <a class="dropdown-item" name="cancel" type="submit" value="cancel" >Cancel</a>
