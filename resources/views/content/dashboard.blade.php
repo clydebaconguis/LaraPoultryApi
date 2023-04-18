fd5e69c993fa18dc2a518260ec5d89d06de6ef09
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-04-18 14:48:27 +0800

    add functionality in dash

--- resources/views/content/dashboard.blade.php
+++ resources/views/content/dashboard.blade.php
@@ -55,7 +55,7 @@
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$totalOrders}}</div>
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$totalOrder}}</div>
                                 </div>
                                 <div class="col">
                                     <div class="progress progress-sm mr-2">
@@ -82,7 +82,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                 Total Users</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalUsers}}</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalUser}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-comments fa-2x text-gray-300"></i>
