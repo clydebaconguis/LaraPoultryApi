978893daaeed912349e84395e8601f8471ca8a4f
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-09 17:18:52 +0800

    add lat long

--- database/migrations/2022_07_20_154522_create_transactions_table.php
+++ database/migrations/2022_07_20_154522_create_transactions_table.php
@@ -17,11 +17,17 @@ class CreateTransactionsTable extends Migration
             $table->increments('id');
             $table->string('trans_code');
             $table->string('user_add');
+            $table->double('lat')->nullable();
+            $table->double('long')->nullable();
             $table->string('phone');
             $table->decimal('total_payment', 8, 2);
             $table->string('payment_opt');
             $table->string('status')->nullable()->default('Approval');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id');
+            $table->date('date_to_deliver')->nullable();
+            $table->date('date_delivered')->nullable();
+            $table->string('proof_of_delivery')->nullable();
+            $table->string('proof_of_payment')->nullable();
             $table->timestamps();
         });
     }
