0960f6dab192312a9df92db60f59a77ee7163a64
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-10 12:50:13 +0800

    editaccount

--- routes/web.php
+++ routes/web.php
@@ -221,14 +221,14 @@ Route::get('/orderdetails/{id}', function ($id) {
     );
 });
 
-Route::get('/editaccount/{id}/edit', function ($id) {
-    return view('content.editaccount', ['detail' => Account::where('id', $id)->get()]);
-});
 
 Route::get('/accounts', function () {
     return view('content.accounts', ['accounts' => Account::all()]);
 });
 
+Route::get('/editaccount/{id}/edit', function ($id) {
+    return view('content.editaccount', ['detail' => Account::where('id', $id)->get()]);
+});
 Route::put('/updateaccount/{id}', function ($id, Request $request) {
 
     $products = $request->validate([
