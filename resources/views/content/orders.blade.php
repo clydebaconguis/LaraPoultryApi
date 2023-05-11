aee5d7e8d91a97d3d4b0729946c5f3e1b8299244
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-12 01:04:55 +0800

    sort orders

--- resources/views/content/orders.blade.php
+++ resources/views/content/orders.blade.php
@@ -37,31 +37,31 @@
                             </tr>
                         </tfoot>
                         <tbody>
-                            <tr>
                              @unless (count($orders) == 0)           
                                 @foreach ($orders as $order)
-                                        <td>{{$order->trans_code}}</td>
-                                        <td>{{$order->name}}</td>
-                                        <td>{{$order->phone}}</td>
-                                        <td>{{$order->total_payment}}</td>
-                                        <td>{{$order->status}}</td>
-                                        <td>{{$order->created_at}}</td>
-                                        <td class="d-flex">
-                                            <a type="button" class="btn btn-info" href="/orderdetails/{{$order->id}}">Details</a>
-                                            <form method="POST" id="myform" action="/orderstat/{{$order->id}}" enctype="multipart/form-data" >
-                                            @csrf
-                                                {{-- <input type="hidden" name="orderid" value={{}}/> --}}
-                                                <select name="orderstat" id="orderstat" class="btn btn-danger ml-1" onchange="this.form.submit()">
-                                                    <option  class="bg-light text-dark" selected>Select status</option>
-                                                    <option  class="bg-light text-dark" value="delivery">Approve</option>
-                                                    <option  class="bg-light text-dark" value="delivered">Delivered</option>
-                                                    <option  class="bg-light text-dark"  value="cancel">Cancel</option>
-                                                </select>
-                                            </form>
-                                        </td>
+                                <tr>
+                                    <td>{{$order->trans_code}}</td>
+                                    <td>{{$order->name}}</td>
+                                    <td>{{$order->phone}}</td>
+                                    <td>{{$order->total_payment}}</td>
+                                    <td>{{$order->status}}</td>
+                                    <td>{{$order->created_at}}</td>
+                                    <td class="d-flex">
+                                        <a type="button" class="btn btn-info" href="/orderdetails/{{$order->id}}">Details</a>
+                                        <form method="POST" id="myform" action="/orderstat/{{$order->id}}" enctype="multipart/form-data" >
+                                        @csrf
+                                            {{-- <input type="hidden" name="orderid" value={{}}/> --}}
+                                            <select name="orderstat" id="orderstat" class="btn btn-danger ml-1" onchange="this.form.submit()">
+                                                <option  class="bg-light text-dark" selected>Select status</option>
+                                                <option  class="bg-light text-dark" value="delivery">Approve</option>
+                                                <option  class="bg-light text-dark" value="delivered">Delivered</option>
+                                                <option  class="bg-light text-dark"  value="cancel">Cancel</option>
+                                            </select>
+                                        </form>
+                                    </td>
+                                </tr>
                                 @endforeach
                             @endunless
-                            </tr>
                         </tbody>
                     </table>
                 </div>
