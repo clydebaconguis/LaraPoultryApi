89c440b172f06b489b64fd89be03cf90a807b9d7
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-12 00:50:51 +0800

    sort orders

--- routes/web.php
+++ routes/web.php
@@ -163,7 +163,7 @@ Route::get('/editprod/{prod}/edit', function (ProductCategory $prod) {
 Route::get('/orders', function () {
     return view('content.orders', ['orders' => Transaction::select('transactions.*','users.name')
         ->join('users', 'transactions.user_id', "=", 'users.id')
-        ->orderBy('created_at', 'desc')->get()]);
+        ->orderBy('id', 'asc')->get()]);
 });
 
 Route::post('/orderstat/{orderid}', function ($orderid, Request $request) {
