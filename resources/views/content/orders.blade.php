dd3671a4929828f02be4475cad003e32c6896280
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-12 01:19:35 +0800

    sort orders

--- resources/views/content/orders.blade.php
+++ resources/views/content/orders.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+a<x-layout>
     <!-- Begin Page Content -->
     <div class="container-fluid">
 
@@ -46,19 +46,18 @@
                                     <td>{{$order->total_payment}}</td>
                                     <td>{{$order->status}}</td>
                                     <td>{{$order->created_at}}</td>
-                                    {{-- <td class="d-flex"> --}}
-                                        {{-- <a type="button" class="btn btn-info" href="/orderdetails/{{$order->id}}">Details</a>
-                                        <form method="POST" id="myform" action="/orderstat/{{$order->id}}" enctype="multipart/form-data" > --}}
-                                        {{-- @csrf --}}
-                                            {{-- <input type="hidden" name="orderid" value={{}}/> --}}
-                                            {{-- <select name="orderstat" id="orderstat" class="btn btn-danger ml-1" onchange="this.form.submit()">
+                                    <td class="d-flex">
+                                        <form method="POST" id="myform" action="/orderstat/{{$order->id}}" enctype="multipart/form-data" >
+                                            @csrf
+                                            <a type="button" class="btn btn-info" href="/orderdetails/{{$order->id}}">Details</a>
+                                            <select name="orderstat" id="orderstat" class="btn btn-danger ml-1" onchange="this.form.submit()">
                                                 <option  class="bg-light text-dark" selected>Select status</option>
                                                 <option  class="bg-light text-dark" value="delivery">Approve</option>
                                                 <option  class="bg-light text-dark" value="delivered">Delivered</option>
                                                 <option  class="bg-light text-dark"  value="cancel">Cancel</option>
-                                            </select> --}}
-                                        {{-- </form> --}}
-                                    {{-- </td> --}}
+                                            </select>
+                                        </form>
+                                    </td>
                                 </tr>
                                 @endforeach
                             @endunless
