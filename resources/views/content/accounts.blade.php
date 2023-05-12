e5389b55f18c067f7f9c6bb5af06691f95ccc415
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-12 14:25:50 +0800

    new admin

--- resources/views/content/accounts.blade.php
+++ resources/views/content/accounts.blade.php
@@ -6,24 +6,38 @@
         <h1 class="h3 mb-4 text-gray-800">Account</h1>
         @if(session()->has('message')) <p class="alert alert-success">{{session('message')}}</p> @endif
 
-        <form  method="POST" action="/addaccount" enctype="multipart/form-data">
+        <form  method="POST" action="/addaccount" class="d-flex" enctype="multipart/form-data">
             @csrf
-            <div class="form-group">
-              <label for="num">Number</label>
-              <input type="text" class="form-control" name="num" id="num" required placeholder="Enter Num">
+            <div>
+                <div class="form-group">
+                    <label for="num">New Gcash</label>
+                    <input type="text" class="form-control" name="num" id="num" required placeholder="Enter Number">
+                </div>
+                <div hidden class="form-group">
+                    <label for="passcode">Passcode</label>
+                    <input type="text" class="form-control" name="passcode" id="passcode" placeholder="Enter Passcode">
+                </div>
             </div>
-            <div hidden class="form-group">
-              <label for="passcode">Passcode</label>
-              <input type="text" class="form-control" name="passcode" id="passcode" placeholder="Enter Passcode">
+            <div>
+                <h2>Add New Admin</h2>
+                <div class="form-group">
+                    <label for="username">Email</label>
+                    <input type="email" class="form-control" name="username" id="username" required placeholder="Enter Email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="text" class="form-control" name="password" id="password" required placeholder="Enter Password">
+                </div>
             </div>
-            <button type="submit" class="mb-4 btn btn-primary">Submit</button>
+            
+            <button type="submit" class="mb-4 btn btn-primary">Add</button>
         </form>
 
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Gcash & Passcode</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Gcash & other payment number</h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
