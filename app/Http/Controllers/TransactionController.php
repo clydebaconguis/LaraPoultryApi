1c2808f74f68aeb7768ea5aeb2eeff8bcbf35b82
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-15 17:33:10 +0800

    pop

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -67,6 +67,9 @@ class TransactionController extends Controller
 
         $formfields['lat'] = $request->lat;
         $formfields['long'] = $request->long;
+        if($request->has('proof_of_payment')){
+            $formfields['proof_of_payment'] = $request->proof_of_payment;
+        }
         $formfields['trans_code'] = Str::random(10);
         $transaction = Transaction::create($formfields);
 
