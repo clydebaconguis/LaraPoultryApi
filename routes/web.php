2959169e7d767f86d093b51f7b32bf538dcccbad
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-22 12:20:29 +0800

    add new table

--- routes/web.php
+++ routes/web.php
@@ -295,11 +295,11 @@ Route::get('/ondelivery', function () {
      [
         'orders' => Transaction::select('transactions.*','users.name')
         ->join('users', 'transactions.user_id', "=", 'users.id')
-        ->where('status', 'delivery')
+        ->where('transactions.status', 'delivery')
         ->orderBy('id', 'desc')->get(),
         'done' => Transaction::select('transactions.*','users.name')
         ->join('users', 'transactions.user_id', "=", 'users.id')
-        ->where('status', 'delivered')
+        ->where('transactions.status', 'delivered')
         ->orderBy('id', 'desc')->get()
     ]);
 })->middleware('auth');
