04d5db9f1b460cafc740f0453f6916633c9711d0
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-23 09:23:18 +0800

    dashboard

--- resources/views/content/dashboard.blade.php
+++ resources/views/content/dashboard.blade.php
@@ -11,10 +11,11 @@
     <div class="row">
 
         <!-- Earnings (Monthly) Card Example -->
-        <a href="/products">
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
+        
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <a href="/products">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
@@ -25,10 +26,11 @@
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
-        </a>
+        </div>
+    
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
