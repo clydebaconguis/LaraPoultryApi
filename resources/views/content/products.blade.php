6d950d38ac8a7e46458fd8ad43d1db68167eb352
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-24 18:38:47 +0800

    enabled button

--- resources/views/content/products.blade.php
+++ resources/views/content/products.blade.php
@@ -44,10 +44,11 @@
                                 @foreach ($products as $product)
                                     <tr>
                                         <td> <image class="rounded-circle justify-content-center" style="width: 50px; height: 50px" src="https://drive.google.com/uc?export=view&id={{$product->image}}" /></td>
-                                        <td class="text-danger">{{$product->name}}</td>
                                         @if ($product->status == 1)
+                                            <td class="text-success">{{$product->name}}</td>
                                             <td class="text-success">Active</td>
                                         @else
+                                            <td class="text-danger">{{$product->name}}</td>
                                             <td class="text-danger">Disable</td>
                                         @endif
                                         <td>{{$product->stock}}</td>
