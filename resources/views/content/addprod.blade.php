58da91d1cec4afb1a5d3ce684bb07f40a51b004c
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-05-24 17:19:46 +0800

    dropdown

--- resources/views/content/addprod.blade.php
+++ resources/views/content/addprod.blade.php
@@ -63,7 +63,7 @@
                         <option selected>Select Unit</option>
                         @unless (count($units) == 0)           
                             @foreach ($units as $unit)
-                                <option value={{$unit->unit}}>{{$unit->unit}} - {{$units['type.name']}}</option>
+                                <option value={{$unit->unit}}>{{$unit->unit}} - {{$unit['type.name']}}</option>
                             @endforeach
                         @endunless
                     </select>
