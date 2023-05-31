db70de2e7da8c9258c895fe4e3db2bdbc85a3269
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-06-01 00:40:39 +0800

    sms servicee

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Twilio\Rest\Client;
 use Illuminate\Http\Request;
 
@@ -9,20 +10,28 @@ class SMSController extends Controller
 {
     public function sendSMS()
     {
-        $twilioSid = getenv('TWILIO_SID');
-        $twilioToken = getenv('TWILIO_AUTH_TOKEN');
-        $twilioPhoneNumber = getenv('TWILIO_PHONE_NUMBER');
-        
-        $client = new Client($twilioSid, $twilioToken);
-        
-        $message = $client->messages->create(
-            '09554587790', // Replace with the recipient's phone number
-            [
-                'from' => $twilioPhoneNumber,
-                'body' => 'This is a test message from Laravel using Twilio!',
-            ]
-        );
-        
+        try{
+
+            $twilioSid = getenv('TWILIO_SID');
+            $twilioToken = getenv('TWILIO_AUTH_TOKEN');
+            $twilioPhoneNumber = getenv('TWILIO_PHONE_NUMBER');
+            
+            $client = new Client($twilioSid, $twilioToken);
+            
+            $message = $client->messages->create(
+                '+63 (955) 458‑7790', // Replace with the recipient's phone number
+                [
+                    'from' => $twilioPhoneNumber,
+                    'body' => 'This is a test message from Laravel using Twilio!',
+                    ]
+                );
+
+                dd("success");
+                
+        }catch(Exception $e){
+
+            dd("Error:". $e->getMessage());
+        }
         // Optionally, you can handle the response or error here
         // For example, you can log the message ID or handle any exceptions
         // $message->sid will give you the message SID
