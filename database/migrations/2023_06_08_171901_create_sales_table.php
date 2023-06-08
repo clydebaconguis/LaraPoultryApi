407c1a2e65f098dc12eb7cd40c3d9d3b6f010b67
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-06-08 17:30:12 +0800

    assign rider

---
+++ database/migrations/2023_06_08_171901_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rider_id');
+            $table->decimal('profit', 8, 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
