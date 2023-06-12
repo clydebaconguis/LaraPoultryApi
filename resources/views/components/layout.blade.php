d5cdc72cc943bcfff5c7cf60f01e2b322ea8698c
Clyde Baconguis <clydebaconguis3737@gmail.com>
2023-06-12 12:08:25 +0800

    twillio set

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,6 +22,9 @@
      <!-- Custom styles for products page -->
     <link href="{{asset('css/assets/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
 <script
